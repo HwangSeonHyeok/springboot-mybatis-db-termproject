cf181b6954b4806f128c7adc49120242b9393375
JamesWithCode <minseok.jeong.james@gmail.com>
2022-11-02 00:13:12 +0900

    initial commit

---
+++ src/main/java/com/team8/dbproject/SpringbootMybatisDbTermprojectApplication.java
@@ -0,0 +1,13 @@
+package com.team8.dbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootMybatisDbTermprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootMybatisDbTermprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/team8/dbproject/SpringbootMybatisDbTermprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.team8.dbproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootMybatisDbTermprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
